fd75999cf813fc2497521dd926f880f519721c2e
Andrew Gray <ayjaygee@users.noreply.github.com>
2024-07-31 21:34:22 +0930

    Will print out a text file

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
+	fileName := os.Args[1]
+	bytes, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", bytes)
 }
