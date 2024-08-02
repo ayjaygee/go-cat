b30cf2cb80851164f35937b3e810583658138ce2
Andrew Gray <ayjaygee@users.noreply.github.com>
2024-08-02 20:38:56 +0930

    Changed options to be set by bit flags and tried to make initialization cleaner

--- main.go
+++ main.go
@@ -8,14 +8,23 @@ import (
 	"strings"
 )
 
+const (
+	COUNT_LINES = 1 << iota
+	EXCLUDE_BLANKS
+	DEFAULT_MODE = 0
+)
+
 type MyWriter struct {
-	dest          io.Writer
-	lineNum       int
-	excludeBlanks bool
-	prevByte      byte
+	dest     io.Writer
+	lineNum  int
+	mode     int
+	prevByte byte
 }
 
 func (out *MyWriter) Write(bytes []byte) (int, error) {
+	if out.mode&COUNT_LINES == 0 {
+		return out.dest.Write(bytes)
+	}
 	for i, b := range bytes {
 		var p []byte
 		if out.prefixRequired(b) {
@@ -37,40 +46,44 @@ func (out *MyWriter) prefixGen() []byte {
 }
 
 func (out *MyWriter) prefixRequired(r byte) bool {
-	return out.prevByte == '\n' && (!out.excludeBlanks || r != '\n')
+	return out.mode&COUNT_LINES != 0 &&
+		out.prevByte == '\n' &&
+		(out.mode&EXCLUDE_BLANKS == 0 || r != '\n')
 }
 
-func NewLineNumberer(dest io.Writer, excludeBlanks bool) io.Writer {
-	return &MyWriter{dest, 1, excludeBlanks, '\n'}
+func NewWriter(dest io.Writer, mode int) io.Writer {
+	return &MyWriter{
+		dest:     dest,
+		lineNum:  1,
+		mode:     mode,
+		prevByte: '\n',
+	}
 }
 
 func main() {
 	args := os.Args[1:]
-	flags := []string{}
 	fileNames := []string{}
-	var out io.Writer = os.Stdout
+	mode := DEFAULT_MODE
 	for _, arg := range args {
 		if strings.HasPrefix(arg, "-") {
-			flags = append(flags, arg)
+			switch arg {
+			case "-":
+				fileNames = append(fileNames, arg)
+			case "-n":
+				mode = mode | COUNT_LINES
+			case "-b":
+				mode = mode | COUNT_LINES | EXCLUDE_BLANKS
+			default:
+				log.Fatalf("unknown flag %s", arg)
+			}
 		} else {
 			fileNames = append(fileNames, arg)
-		}
-	}
-	for _, flag := range flags {
-		switch flag {
-		case "-":
-			fileNames = append(fileNames, flag)
-		case "-n":
-			out = NewLineNumberer(os.Stdout, false)
-		case "-b":
-			out = NewLineNumberer(os.Stdout, true)
-		default:
-			log.Fatalf("unknown flag %s", flag)
 		}
 	}
 	if len(fileNames) == 0 {
 		fileNames = []string{"-"}
 	}
+	out := NewWriter(os.Stdout, mode)
 	err := IterateAndOutput(fileNames, out)
 	if err != nil {
 		log.Fatal(err)
